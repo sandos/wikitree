197e63429309db2afc9d4d554597fb0ef326aaf5
John Bäckstrand <sopues@gmail.com>
2023-10-08 00:22:59 +0200

    Update api_test.py

--- api_test.py
+++ api_test.py
@@ -1,16 +1,28 @@
 import requests
+from datetime import datetime as dt
 
 url = 'https://api.wikitree.com/api.php'
 
 params = dict(
     action='getAncestors',
     key='Bäckstrand-5',
-    depth="2",
+    depth="1",
     appId='jb_test_1'
 )
 
+now = dt.now()
 resp = requests.get(url=url, params=params)
 data = resp.json() # Check the JSON Response Content documentation below
+print(f"Took {dt.now()-now}")
+
+print(data)
+
+num = 0
 
 for p in data[0]["ancestors"]:
     print(p["Name"], p["Id"], p["Father"], p["Mother"])
+    num += 1
+
+
+print(data[0]["ancestors"][0])
+print(f"Number {num}")
